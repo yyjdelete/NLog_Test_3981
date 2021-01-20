11499918328250a41b930828b91a064083addfa5
SilverFox <yyjdelete@126.com>
2021-01-20 13:30:38 +0800

    test

---
+++ Loader/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace Loader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var aaa = Path.GetFullPath(Path.Combine(typeof(Program).Assembly.Location, @"../../../../../NLogTest/bin/Debug/netcoreapp3.1/NLogTest.dll"));
+            var loader = new PluginLoadContext(aaa);
+            var ass = loader.LoadFromAssemblyPath(aaa);
+            var pg2 = ass.GetType("NLogTest.Program");
+            pg2.GetMethod("Main", BindingFlags.Static | BindingFlags.NonPublic).Invoke(null, new object[] { Array.Empty<string>() });
+        }
+        private class PluginLoadContext : System.Runtime.Loader.AssemblyLoadContext
+        {
+            private readonly System.Runtime.Loader.AssemblyDependencyResolver _resolver;
+
+            public PluginLoadContext(string pluginPath)
+            {
+                _resolver = new System.Runtime.Loader.AssemblyDependencyResolver(pluginPath);
+            }
+
+            protected override Assembly Load(AssemblyName assemblyName)
+            {
+                string assemblyPath = _resolver.ResolveAssemblyToPath(assemblyName);
+                if (assemblyPath != null)
+                {
+                    return LoadFromAssemblyPath(assemblyPath);
+                }
+
+                return null;
+            }
+
+            protected override IntPtr LoadUnmanagedDll(string unmanagedDllName)
+            {
+                string libraryPath = _resolver.ResolveUnmanagedDllToPath(unmanagedDllName);
+                if (libraryPath != null)
+                {
+                    return LoadUnmanagedDllFromPath(libraryPath);
+                }
+
+                return IntPtr.Zero;
+            }
+        }
+    }
+}
---
+++ NLogTest/Program.cs
@@ -0,0 +1,47 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Loader;
+
+namespace NLogTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DumpNLogAssembly("Pre(all):", AppDomain.CurrentDomain.GetAssemblies());
+            DumpNLogAssembly("Pre(default ctx):", AssemblyLoadContext.Default.Assemblies);
+            DumpNLogAssembly("Pre(current ctx):", AssemblyLoadContext.GetLoadContext(typeof(Program).Assembly).Assemblies);
+            //NLog.Common.InternalLogger.LogToConsole = true;
+            //NLog.Common.InternalLogger.LogLevel = NLog.LogLevel.Debug;
+            //var lf = new NLog.LogFactory();
+            //lf.Setup().SetupExtensions(
+            NLog.LogManager.LogFactory.Setup().SetupExtensions(ext => {
+                //ext.RegisterAssemblyLoader(asmName => {
+                //    var assemblyName = new AssemblyName() { Name = asmName };
+                //    var assemblyContext = AssemblyLoadContext.GetLoadContext(typeof(NLog.LogFactory).Assembly);
+                //    return assemblyContext.LoadFromAssemblyName(assemblyName);
+                //});
+                //ext.AutoLoadExtensions();
+                //ext.AutoLoadAssemblies(false);
+            });
+            NLog.LogManager.GetCurrentClassLogger().Info("Start NLog");
+            DumpNLogAssembly("Post(all):", AppDomain.CurrentDomain.GetAssemblies());
+            DumpNLogAssembly("Post(default ctx):", AssemblyLoadContext.Default.Assemblies);
+            DumpNLogAssembly("Post(current ctx):", AssemblyLoadContext.GetLoadContext(typeof(Program).Assembly).Assemblies);
+        }
+
+        private static void DumpNLogAssembly(string title, IEnumerable<Assembly> assemblies)
+        {
+            assemblies = assemblies.Where(a => a.GetName().Name.StartsWith("NLog"));
+            Console.WriteLine();
+            Console.WriteLine(title + assemblies.Count());
+            foreach (var cur in assemblies)
+            {
+                Console.WriteLine(cur.FullName);
+            }
+        }
+    }
+}
